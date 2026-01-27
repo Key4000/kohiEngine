51f28d7814099759dca43d5e6db3a522fd2e5765
Павел <107314499+Key4000@users.noreply.github.com>
2026-01-27 20:25:27 +0500

    Update defines.h

--- engine/src/defines.h
+++ engine/src/defines.h
@@ -50,45 +50,57 @@ STATIC_ASSERT(sizeof(f64) == 8, "Expected f64 to be 8 bytes.");
 #ifndef _WIN64
 #error "64-bit is required on Windows!"
 #endif
+
+
 #elif defined(__linux__) || defined(__gnu_linux__)
 // Linux OS
 #define KPLATFORM_LINUX 1
 #if defined(__ANDROID__)
 #define KPLATFORM_ANDROID 1
 #endif
+
+
 #elif defined(__unix__)
-// Catch anything not caught by the above.
+// Catch(поймать) anything not(всё что не) caught(поймали) by the above(выше).
 #define KPLATFORM_UNIX 1
+
 #elif defined(_POSIX_VERSION)
 // Posix
 #define KPLATFORM_POSIX 1
+
 #elif __APPLE__
 // Apple platforms
 #define KPLATFORM_APPLE 1
+
 #include <TargetConditionals.h>
 #if TARGET_IPHONE_SIMULATOR
 // iOS Simulator
 #define KPLATFORM_IOS 1
 #define KPLATFORM_IOS_SIMULATOR 1
+
 #elif TARGET_OS_IPHONE
 #define KPLATFORM_IOS 1
 // iOS device
+
 #elif TARGET_OS_MAC
 // Other kinds of Mac OS
 #else
 #error "Unknown Apple platform"
 #endif
+
 #else
 #error "Unknown platform!"
 #endif
 
 #ifdef KEXPORT
+
 // Exports
 #ifdef _MSC_VER
 #define KAPI __declspec(dllexport)
 #else
 #define KAPI __attribute__((visibility("default")))
 #endif
+
 #else
 // Imports
 #ifdef _MSC_VER
@@ -96,5 +108,6 @@ STATIC_ASSERT(sizeof(f64) == 8, "Expected f64 to be 8 bytes.");
 #else
 #define KAPI
 #endif
+
 #endif
 
