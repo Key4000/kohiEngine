d15c68daf68559764e1636f9de7b6a65a02f4efe
Key4000 <pvlzhizhin@mail.ru>
2026-01-30 20:20:01 +0500

    fix all problems

--- engine/src/core/asserts.h
+++ engine/src/core/asserts.h
@@ -1,72 +1,86 @@
 /*
-Этот код описывает систему утверждений (assertions) для проверки корректности работы программы на этапе выполнения. Он включает механизмы для проверки условий (asserts) и обработки ошибок с использованием дебаггеров и сообщений об ошибках. 
+Этот код описывает систему утверждений (assertions) для проверки корректности
+работы программы на этапе выполнения. Он включает механизмы для проверки условий
+(asserts) и обработки ошибок с использованием дебаггеров и сообщений об ошибках.
 */
 
 #pragma once
 
-#include "defines.h"
+#include "../defines.h"
 
-// Если этот макрос закомментировать, то все проверки утверждений будут отключены, и код в блоках #ifdef KASSERTIONS_ENABLED не будет выполняться. Это удобно для отключения утверждений в производственных сборках, где производительность важнее, чем детальная диагностика.
+// Если этот макрос закомментировать, то все проверки утверждений будут
+// отключены, и код в блоках #ifdef KASSERTIONS_ENABLED не будет выполняться.
+// Это удобно для отключения утверждений в производственных сборках, где
+// производительность важнее, чем детальная диагностика.
 #define KASSERTIONS_ENABLED
 
-//Макрос debugBreak используется для принудительного вызова точки останова (breakpoint) в отладчике при ошибке. Это поможет разработчику остановить выполнение программы на момент, когда условие утверждения не выполнено.
+//Макрос debugBreak используется для принудительного вызова точки останова
+//(breakpoint) в отладчике при ошибке. Это поможет разработчику остановить
+//выполнение программы на момент, когда условие утверждения не выполнено.
 #ifdef KASSERTIONS_ENABLED
 #if _MSC_VER
-//На Windows (MSVC) используется встроенная функция __debugbreak(), которая вызывает точку останова, если отладчик подключен. 
+//На Windows (MSVC) используется встроенная функция __debugbreak(), которая
+//вызывает точку останова, если отладчик подключен.
 #include <intrin.h>
 #define debugBreak() __debugbreak()
 #else
-//На Linux и других UNIX-подобных системах используется __builtin_trap(), которая заставляет программу завершиться или войти в отладчик.
+//На Linux и других UNIX-подобных системах используется __builtin_trap(),
+//которая заставляет программу завершиться или войти в отладчик.
 #define debugBreak() __builtin_trap()
 #endif
 
-//Эта функция будет вызываться, когда одно из утверждений не будет выполнено. Она принимает:
-//expression: саму строку с выражением, которое проверяется (например, a == b).
-//message: дополнительное сообщение об ошибке.
-//file: имя файла, в котором произошла ошибка.
-//line: строка, на которой произошла ошибка. 
-KAPI void report_assertion_failure(const char* expression, const char* message, const char* file, i32 line);
+//Эта функция будет вызываться, когда одно из утверждений не будет выполнено.
+//Она принимает: expression: саму строку с выражением, которое проверяется
+// (например, a == b). message: дополнительное сообщение об ошибке. file: имя
+// файла, в котором произошла ошибка. line: строка, на которой произошла ошибка.
+KAPI void report_assertion_failure(const char *expression, const char *message,
+                                   const char *file, i32 line);
 
+//Проверяет условие expr. Если условие не выполнено (т.е. выражение ложно),
+//вызывается функция report_assertion_failure, и выполнение программы
+//останавливается с помощью debugBreak().
+#define KASSERT(expr)                                                          \
+  {                                                                            \
+    if (expr) {                                                                \
+    } else {                                                                   \
+      report_assertion_failure(#expr, "", __FILE__, __LINE__);                 \
+      debugBreak();                                                            \
+    }                                                                          \
+  }
 
-//Проверяет условие expr. Если условие не выполнено (т.е. выражение ложно), вызывается функция report_assertion_failure, и выполнение программы останавливается с помощью debugBreak(). 
-#define KASSERT(expr)                                                \
-    {                                                                \
-        if (expr) {                                                  \
-        } else {                                                     \
-            report_assertion_failure(#expr, "", __FILE__, __LINE__); \
-            debugBreak();                                            \
-        }                                                            \
-    }
+//Это вариант макроса с дополнительным сообщением message, которое будет
+//передано в функцию report_assertion_failure в случае ошибки. Таким образом,
+//разработчик может дать дополнительное объяснение, что именно пошло не так.
+#define KASSERT_MSG(expr, message)                                             \
+  {                                                                            \
+    if (expr) {                                                                \
+    } else {                                                                   \
+      report_assertion_failure(#expr, message, __FILE__, __LINE__);            \
+      debugBreak();                                                            \
+    }                                                                          \
+  }
 
-
-//Это вариант макроса с дополнительным сообщением message, которое будет передано в функцию report_assertion_failure в случае ошибки. Таким образом, разработчик может дать дополнительное объяснение, что именно пошло не так.
-#define KASSERT_MSG(expr, message)                                        \
-    {                                                                     \
-        if (expr) {                                                       \
-        } else {                                                          \
-            report_assertion_failure(#expr, message, __FILE__, __LINE__); \
-            debugBreak();                                                 \
-        }                                                                 \
-    }
-    
 //Только для отладки
 #ifdef _DEBUG
-//В режиме отладки (_DEBUG определён) макрос будет работать как обычный KASSERT — проверяя выражение и вызывая точку останова. 
-#define KASSERT_DEBUG(expr)                                          \
-    {                                                                \
-        if (expr) {                                                  \
-        } else {                                                     \
-            report_assertion_failure(#expr, "", __FILE__, __LINE__); \
-            debugBreak();                                            \
-        }                                                            \
-    }
+//В режиме отладки (_DEBUG определён) макрос будет работать как обычный KASSERT
+//— проверяя выражение и вызывая точку останова.
+#define KASSERT_DEBUG(expr)                                                    \
+  {                                                                            \
+    if (expr) {                                                                \
+    } else {                                                                   \
+      report_assertion_failure(#expr, "", __FILE__, __LINE__);                 \
+      debugBreak();                                                            \
+    }                                                                          \
+  }
 #else
-#define KASSERT_DEBUG(expr)  // Does nothing at all
+#define KASSERT_DEBUG(expr) // Does nothing at all
 #endif
 #else
 
-//Если макрос KASSERTIONS_ENABLED будет закомментирован, то все макросы утверждений (KASSERT, KASSERT_MSG, KASSERT_DEBUG) не будут ничего делать (то есть станут пустыми).
-#define KASSERT(expr)               // Does nothing at all
-#define KASSERT_MSG(expr, message)  // Does nothing at all
-#define KASSERT_DEBUG(expr)         // Does nothing at all
+//Если макрос KASSERTIONS_ENABLED будет закомментирован, то все макросы
+//утверждений (KASSERT, KASSERT_MSG, KASSERT_DEBUG) не будут ничего делать (то
+//есть станут пустыми).
+#define KASSERT(expr) // Does nothing at all
+#define KASSERT_MSG(expr, message) // Does nothing at all
+#define KASSERT_DEBUG(expr) // Does nothing at all
 #endif
--- engine/src/defines.h
+++ engine/src/defines.h
@@ -1,38 +1,35 @@
 #pragma once
 
 // Беззнаковые целые
-typedef unsigned char u8;        // 8 бит 
-typedef unsigned short u16;      // 16 бит
-typedef unsigned int u32;        // 32 бита
-typedef unsigned long long u64;  // 64 бита 
-
+typedef unsigned char u8;       // 8 бит
+typedef unsigned short u16;     // 16 бит
+typedef unsigned int u32;       // 32 бита
+typedef unsigned long long u64; // 64 бита
 
 // Знаковые целые
-typedef signed char i8;          //8 бит 
-typedef signed short i16;        //16 бит 
-typedef signed int i32;          //32 бита 
-typedef signed long long i64;    //64 бита
+typedef signed char i8;       // 8 бит
+typedef signed short i16;     // 16 бит
+typedef signed int i32;       // 32 бита
+typedef signed long long i64; // 64 бита
 
 // Числа с плавающей точкой
-ttypedef float f32;   // 32 бита
-typedef double f64;  // 64 бита
+typedef float f32;  // 32 бита
+typedef double f64; // 64 бита
 
-// Логические типы 
-int b32;   // логическое значение (32 бита)
-typedef char b8;   // логическое значение (8 бит)
+// Логические типы
+typedef int b32; // логическое значение (32 бита)
+typedef char b8; // логическое значение (8 бит)
 
-
-
-//STATIC_ASSERT — статические проверки
+// STATIC_ASSERT — статические проверки
 #if defined(__clang__) || defined(__gcc__)
 #define STATIC_ASSERT _Static_assert
 #else
 #define STATIC_ASSERT static_assert
 #endif
 
-
-//Проверка размеров типов
-//Если компилятор или платформа ведёт себя неожиданно — компиляция упадёт сразу, а не создаст скрытые баги. 
+// Проверка размеров типов
+// Если компилятор или платформа ведёт себя неожиданно — компиляция упадёт
+// сразу, а не создаст скрытые баги.
 STATIC_ASSERT(sizeof(u8) == 1, "Expected u8 to be 1 byte.");
 STATIC_ASSERT(sizeof(u16) == 2, "Expected u16 to be 2 bytes.");
 STATIC_ASSERT(sizeof(u32) == 4, "Expected u32 to be 4 bytes.");
@@ -46,76 +43,32 @@ STATIC_ASSERT(sizeof(i64) == 8, "Expected i64 to be 8 bytes.");
 STATIC_ASSERT(sizeof(f32) == 4, "Expected f32 to be 4 bytes.");
 STATIC_ASSERT(sizeof(f64) == 8, "Expected f64 to be 8 bytes.");
 
-//
 #define TRUE 1
 #define FALSE 0
 
 /* Определение платформы */
-//Определяет Windows 
-//Запрещает 32-битную сборку → движок строго 64-bit 
-#if defined(WIN32) || defined(_WIN32) || defined(__WIN32__) 
+// Определяет Windows
+// Запрещает 32-битную сборку → движок строго 64-bit
+#if defined(WIN32) || defined(_WIN32) || defined(__WIN32__)
 #define KPLATFORM_WINDOWS 1
 #ifndef _WIN64
 #error "64-bit is required on Windows!"
 #endif
 
-//Linux / Android 
-#elif defined(__linux__) || defined(__gnu_linux__)
-#define KPLATFORM_LINUX 1
-
-#if defined(__ANDROID__)
-#define KPLATFORM_ANDROID 1
-#endif
-
-//Unix / POSIX
-#elif defined(__unix__)
-#define KPLATFORM_UNIX 1
-
-#elif defined(_POSIX_VERSION)
-#define KPLATFORM_POSIX 1
-
-//Apple (macOS / iOS)
-#elif __APPLE__
-#define KPLATFORM_APPLE 1
-#include <TargetConditionals.h>
-#if TARGET_IPHONE_SIMULATOR
-#define KPLATFORM_IOS 1
-#define KPLATFORM_IOS_SIMULATOR 1
-#elif TARGET_OS_IPHONE
-#define KPLATFORM_IOS 1
-#elif TARGET_OS_MAC
-#else
-#error "Unknown Apple platform"
-#endif
-
-//Если платформа неизвестна
 #else
 #error "Unknown platform!"
 #endif
 
-/* Если мы собираем библиотеку */
 #ifdef KEXPORT
-
-//MSVC (Windows)
 #ifdef _MSC_VER
-//MSVC (и другие компиляторы для Windows) требует указания, какие символы (функции, переменные) должны быть экспортированы, и __declspec(dllexport) говорит компилятору, что функция или объект будет доступен за пределами DLL.
-  #define KAPI __declspec(dllexport)
-//GCC / Clang
 #else
-//В отличие от Windows, где для экспорта нужно использовать __declspec(dllexport), на Unix-подобных системах используется механизм видимости символов. 
-//__attribute__((visibility("default"))) позволяет сделать символ видимым для других программ.
-//При компиляции динамических библиотек по умолчанию все символы скрыты, и только те, что явно помечены как видимые (с помощью этой атрибуты или -fvisibility=default), экспортируются.
 #define KAPI __attribute__((visibility("default")))
 #endif
-
-/*Если мы используем библиотеку */
 #else
 #ifdef _MSC_VER
-//Компилятор MSVC использует __declspec(dllimport) для того, чтобы при компиляции кода правильно обработать внешние символы, которые будут разрешены в момент линковки с динамической библиотекой.
-#define KAPI __declspec(dllimport)
+#define KAPI __declspec(dllexport)
 #else
-#define KAPI
+#define KAPI __declspec(dllimport)
 #endif
 
 #endif
-
