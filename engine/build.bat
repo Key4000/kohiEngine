542dd6d6d564a7889c4f3bb6435786980821594a
Павел <107314499+Key4000@users.noreply.github.com>
2026-02-02 11:47:52 +0500

    Added absolute path for testebed main

--- engine/build.bat
+++ engine/build.bat
@@ -16,7 +16,7 @@ SET compilerFlags=-g -shared -Wvarargs -Wall -Werror
 
 REM -Wall -Werror
 
-SET includeFlags=-Isrc -I%VULKAN_SDK%/Include
+SET includeFlags=-I%VULKAN_SDK%/Include -I../../engine/src
 SET linkerFlags=-luser32 -lvulkan-1 -L%VULKAN_SDK%/Lib
 SET defines=-D_DEBUG -DKEXPORT -D_CRT_SECURE_NO_WARNINGS
 
